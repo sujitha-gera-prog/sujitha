aa58786b67c79ca3324d6f01fb9045b8834a4866
sujitha <sujithagera@gmail.com>
2024-12-10 10:03:39 +0530

    device drivers

--- LDD_practice/tasks/file1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void global_function() {  // Default: external linkage
-    printf("This is a global function.\n");
-}
-
-static void static_function() {  // Internal linkage
-    printf("This is a static function.\n");
-}
--- LDD_practice/tasks/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-extern void global_function();  // Declaration of global_function from file1.c
-
-int main() {
-    global_function();
-    // static_function();  // Uncommenting this will cause a linker error
-    return 0;
-}
